399c8d49a1168f7d362ffab324fdbddd2eff3b03
Andrew Walters <andrewjpwalters@gmail.com>
2022-04-10 02:27:20 -0500

    commit tree 3b28f734e64c953dbdccbdcf9b63afabf3bf82b0

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+let body = document.querySelector('body')
+let div = document.createElement('div')
+function submitData(username, userEmail) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: username,
+            email: userEmail,
+        })
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (object) {
+            div.textContent = object.id
+            return body.append(div)
+        })
+        .catch(function (error) {
+            div.textContent = error.message
+            return body.append(div)
+        })
+}
